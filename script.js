3c8e3a149383b237026f71050741d97bae20beaf
Muhammad-Waleed381 <waleedraja2008@gmail.com>
2025-02-27 21:34:38 +0500

    Added the javascript logic for input  validation using regex.

--- script.js
+++ script.js
@@ -1,4 +1,44 @@
-let password_input=document.getElementById("password_box");
-password_input.addEventListener("keydown",function(event)){
-    
-}
+let password_input = document.getElementById("input_Field");
+let messageDisplay = document.getElementById("Message_display");
+
+password_input.addEventListener("keydown", function(event) {
+    if(event.key === "Enter") {
+        while(messageDisplay.firstChild){
+            messageDisplay.removeChild(messageDisplay.firstChild);
+        }
+        
+        let Capital = /[A-Z]/.test(password_input.value);
+        let Small = /[a-z]/.test(password_input.value);
+        let Number = /[0-9]/.test(password_input.value);
+        let Special=/[^A-Za-z0-9\s]/g;
+
+
+        try {
+            if(password_input.value.length < 8) {
+                throw new Error("The password must be at least 8 characters long.");
+            }
+            if(!Capital) {
+                throw new Error("The password must contain at least one capital letter.");
+            }
+            if(!Small) {
+                throw new Error("The password must contain at least one lowercase letter.");
+            }
+            if(!Number) {
+                throw new Error("The password must contain at least one number.");
+            }
+            if(!Special){
+                throw new Error("The password must contain at least one Special character.")
+            }
+            let Display=document.createElement("p");
+            Display.textContent = "The password passes all the required validation checks. Well done.";
+            Display.style.color = "green";
+            messageDisplay.appendChild(Display)
+
+        } catch(error) {
+            let Display=document.createElement("p");
+            messageDisplay.textContent = error.message;
+            messageDisplay.style.color = "red";
+            messageDisplay.appendChild(Display);
+        }
+    }
+});
